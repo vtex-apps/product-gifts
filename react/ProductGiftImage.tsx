e645bab4e388c44db753696afd84e0917488b874
victorhmp <victor2142@gmail.com>
2020-02-19 19:05:26 -0300

    Initial implementation of ProductGiftImage component

---
+++ react/ProductGiftImage.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useContext } from 'react'
+import { Image } from 'vtex.store-image'
+
+import { GiftContext } from './components/ProductGift'
+
+interface Props {
+  maxWidth?: string | number
+  maxHeight?: string | number
+  minWidth?: string | number
+  minHeight?: string | number
+  imageIndex?: number
+}
+
+const DEFAULT_IMAGE_DIMENSIONS = 125
+
+const ProductGiftImage: FC<Props> = ({
+  maxWidth = DEFAULT_IMAGE_DIMENSIONS,
+  maxHeight = DEFAULT_IMAGE_DIMENSIONS,
+  minWidth = DEFAULT_IMAGE_DIMENSIONS,
+  minHeight = DEFAULT_IMAGE_DIMENSIONS,
+  imageIndex = 0,
+}) => {
+  const gift = useContext(GiftContext)
+  return (
+    <Image
+      maxWidth={maxWidth}
+      maxHeight={maxHeight}
+      minWidth={minWidth}
+      minHeight={minHeight}
+      src={gift?.images[imageIndex].imageUrl}
+      alt={gift?.productName}
+      title={gift?.productTitle}
+    />
+  )
+}
+
+export default ProductGiftImage
