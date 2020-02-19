219d335ab1f2fbbeb9601f29009eef542f1be7b0
victorhmp <victor2142@gmail.com>
2020-02-19 19:05:57 -0300

    Initial implementation of ProductGiftList component

---
+++ react/ProductGiftList.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react'
+import { useCssHandles } from 'vtex.css-handles'
+
+import { useProductGiftsState } from './ProductGiftsContext'
+import ProductGift from './components/ProductGift'
+
+const CSS_HANDLES = ['productGiftListContainer']
+
+const ProductGiftList: FC = ({ children }) => {
+  const { gifts, maxVisibleItems } = useProductGiftsState()
+  const handles = useCssHandles(CSS_HANDLES)
+  const itemsToShow =
+    maxVisibleItems === 'showAll' ? gifts.length : maxVisibleItems
+
+  return (
+    <div className={`${handles.productGiftListContainer} mv5`}>
+      {gifts.slice(0, itemsToShow).map((gift, idx) => (
+        <ProductGift key={gift.linkText} giftIndex={idx}>
+          {children}
+        </ProductGift>
+      ))}
+    </div>
+  )
+}
+
+export default ProductGiftList
