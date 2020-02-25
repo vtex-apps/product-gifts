42fcf4f6cbc19375e02b8ca1ca82420d5c415160
victorhmp <victor2142@gmail.com>
2020-02-25 10:47:14 -0300

    Add testing for CSS handles API

--- react/__tests__/CSSHandlesAPI.test.tsx
+++ react/__tests__/CSSHandlesAPI.test.tsx
@@ -23,6 +23,11 @@ const CSS_HANDLES_API = [
   'giftListItem',
 ]
 
+/**
+ * It is not good practice to test the CSS and it goes against
+ * the philosophy behind react-testing-library, but it is useful in our
+ * case because our CSS handles are a public API.
+ */
 describe('CSS handles API', () => {
   it('should have all expected CSS handles', () => {
     const { container } = renderWithProductContext(
